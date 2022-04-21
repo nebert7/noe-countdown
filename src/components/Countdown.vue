42ad95b305a464b8cfa83196c73ce9e60a04c335
nebert7 <32460543+nebert7@users.noreply.github.com>
2022-04-21 12:06:09 -0400

    [RACCOON] use raccoon favicon, adjust date and variable names

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="display-5">
-      {{ daysLeft }}
+      {{ daysLeftCount }}
     </h1>
     <p>{{ $t('daysLeft') }}</p>
   </div>
@@ -11,8 +11,8 @@
 export default {
   name: 'Countdown',
   computed: {
-    daysLeft() {
-      var meeting = new Date("07/11/2022");
+    daysLeftCount() {
+      var meeting = new Date("07/10/2022");
       var Difference_In_Time = meeting.getTime() - Date.now();
       var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
       return Math.ceil(Difference_In_Days);
