8f3dd799073fb5cc51d57c89bbd1d853497c2baf
Noah Ebert <nebert@updox.com>
2023-02-12 12:41:03 -0500

    [VALENTINES] - 2023

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,6 +8,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
+            <router-link to="/valentines" class="nav-link">Valentine's Day</router-link>
+          </li>
+          <li class="nav-item">
             <router-link to="/" class="nav-link active" aria-current="page">{{ $t('countdown') }}</router-link>
           </li>
           <li class="nav-item">
